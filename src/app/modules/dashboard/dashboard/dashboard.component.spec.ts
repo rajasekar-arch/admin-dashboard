cbadba0d7e2c23431799eaf71560f40b96d9034f
Rajasekar <erajasekargv@gmail.com>
2025-04-06 21:36:16 +0530

    Production: spec file is updated

--- src/app/modules/dashboard/dashboard/dashboard.component.spec.ts
+++ src/app/modules/dashboard/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterModule } from '@angular/router';
+import { MaterialModule } from '../../material-module/material/material.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -8,7 +12,15 @@ describe('DashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent],
+      imports: [
+        DashboardComponent,
+        CommonModule,
+        RouterModule,
+        RouterLink,
+        MaterialModule,
+        RouterTestingModule,
+      ],
+      providers: [],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
@@ -18,5 +30,13 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should check the menuListItems defined', () => {
+    expect(component.menuListItems).toBeTruthy();
+  });
+
+  it('should check the menuListItems should have length', () => {
+    expect(component.menuListItems.length).toBeGreaterThan(0);
   });
 });
--- src/app/modules/dashboard/users-list/users-list.component.spec.ts
+++ src/app/modules/dashboard/users-list/users-list.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UsersListComponent } from './users-list.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from '../../material-module/material/material.module';
 
 describe('UsersListComponent', () => {
   let component: UsersListComponent;
@@ -8,7 +11,7 @@ describe('UsersListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UsersListComponent],
+      imports: [UsersListComponent, CommonModule, HttpClientModule, MaterialModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(UsersListComponent);
@@ -18,5 +21,19 @@ describe('UsersListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should displayedColumns defined', () => {
+    expect(component.displayedColumns).toBeDefined();
+  });
+
+  it('should dataSource defined', () => {
+    expect(component.dataSource).toBeDefined();
+  });
+
+  it('should called on ngOnInit', () => {
+    const spy = spyOn(component, 'getUsersList');
+    component.ngOnInit();
+    expect(spy).toHaveBeenCalled();
   });
 });
