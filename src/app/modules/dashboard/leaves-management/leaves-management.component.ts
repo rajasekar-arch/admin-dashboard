f1e5d3ae23c0a64b7419713bace7b0176ec65a6b
Rajasekar <erajasekargv@gmail.com>
2025-04-21 22:34:44 +0530

    Production: Leave management is inprogress

--- src/app/modules/dashboard/leaves-management/leaves-management.component.ts
+++ src/app/modules/dashboard/leaves-management/leaves-management.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../material-module/material/material.module';
 
 @Component({
   selector: 'app-leaves-management',
   templateUrl: './leaves-management.component.html',
   styleUrl: './leaves-management.component.scss',
+  imports: [CommonModule, MaterialModule],
 })
 export class LeavesManagementComponent {}
