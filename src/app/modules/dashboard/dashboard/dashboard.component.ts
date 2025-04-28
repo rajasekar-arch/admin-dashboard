d6e37c220097dbc52a668c5fdc17e3d70e5d3e0a
Rajasekar <erajasekargv@gmail.com>
2025-04-28 13:47:53 +0530

    admin-dashboard: charts are added

--- src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -1,12 +1,64 @@
 import { Component } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../material-module/material/material.module';
+import { BaseChartDirective } from 'ng2-charts';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { ChartConfiguration, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule, BaseChartDirective],
+  providers: [provideCharts(withDefaultRegisterables())],
   standalone: true,
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  public totalUsers = 1200;
+  public activeUsers = 875;
+  public newUsers = 150;
+
+  // Pie
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: false,
+  };
+  public pieChartLabels = [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
+  public pieChartDatasets = [
+    {
+      data: [300, 500, 100],
+    },
+  ];
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
+
+  
+  // Line chart data
+
+  public lineChartData: ChartConfiguration<'line'>['data'] = {
+    labels: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July'
+    ],
+    datasets: [
+      {
+        data: [ 65, 59, 80, 81, 56, 55, 40 ],
+        label: 'Series A',
+        fill: true,
+        tension: 0.5,
+        borderColor: 'black',
+        backgroundColor: 'rgba(255,0,0,0.3)'
+      }
+    ]
+  };
+  public lineChartOptions: ChartOptions<'line'> = {
+    responsive: false
+  };
+  public lineChartLegend = true;
+
+}
